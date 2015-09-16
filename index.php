33990e80eafa375164d8622786e8c754a7ef8837
kentucky-roberts <croberts@lknpro.com>
2015-09-16 10:10:44 -0400

    bootstrapped index

--- index.php
+++ index.php
@@ -19,8 +19,54 @@
 
     <div class="container">
       <div class="row">
-        <h1>LKNcleaning.com</h1>
-        <section class="col-lg-12">Welcome to our website.</section>
+        <section class="col-lg-12">
+
+          <p class="lead text-center">
+            "Avoid Uneducated, Uninformed, & Sometimes Downright Unscrupulous Cleaners!"<br/>
+            Instead, Get the Most Outstanding Service Experience Ever With LKN Cleaning!
+          </p>
+
+          <h3>4 Important reasons to choose LKN Cleaning</h3>
+          <p>            
+            <ol>
+              <li><strong>Reputation</strong><br/>LKN Cleaning enjoys a reputation that is second to none. Some of the areas most seasoned carpet professionals, interior designers, and real estate agents refer our services exclusively.</li>
+              <li><strong>Experience</strong> LKN Cleaning is experienced in all methods of carpet cleaning, all fabric types and carpet construction as well as the most unusual soiling conditions. This gives you the confidence that we will be knowledgeable of your situation regardless of the situation.</li>
+              <li><strong>Systems</strong> LKN cleaning is committed to first class customer service. You will be treated with the utmost of respect. Will use the most advanced cleaning systems on the market to get maximum soil removal and maximum spot removal without over-wetting your carpet or leaving a sticky residue behind.</li>
+              <li><strong>Guarantee</strong> If you are not completely thrilled with the service experience you receive from us, we will rush back to your location at no charge and no obligation.</li>
+            </ol>
+          </p>
+
+          <h3>Types of Carpet</h3>
+          <p>
+            <strong>Nylon:</strong> Nylon is very durable, resilient and soil-resist it. It offers good colorfastness. Nylon tends to stain easily and dries slowly after cleaning.<br/>
+
+            <strong>Olefin:</strong> Olefin is naturally stain, fade and moisture-resistant and is extremely colorfast. It cleans easily and dries very quickly compared to most fibers.<br/>
+
+            <strong>Polyester:</strong> Polyester is naturally stain and fade-resistant and offers exceptional softness in color clarity. Polyester is not as resilient as nylon and yellows with repeated exposure to petroleum-based spotters.<br/>
+
+            <strong>Acrylic:</strong> Popular fiber in the 1960's since it looks and feels like wool. It tends to fill (fuzz), so it is seldom used.<br/>
+
+            <strong>Wool:</strong> Wool is a natural fiber that does not offer the same durability and resilience as nylon. Wool is not exceptionally stain or soil-resistant and should only be cleaned with natural pH detergents. Always avoid excess moisture when cleaning.<br/>
+          </p>
+
+
+
+          <p>
+            <h3>You can depend on LKN Cleaning for:</h3>
+            <ul>
+              <li>Professional Carpet Cleaning - Spot/Spray - Extract/Rinse - Groom/Dry</li>
+              <li>Oriental & Area Rug Cleaning - Dust - Wash - Rinse - Dry</li>
+              <li>Carpet & Rug Protectant - Moth Repel - Air Dust - Teflon Application</li>
+              <li>Urine Damage Treatment - Spray/Extract - Neutralize pH - Rinse/Groom</li>
+              <li>Expert Upholstery Cleaning - Couches/Sofas/Tapestries/Drapery</li>
+            </ul>
+
+
+
+
+    
+          </p>
+        </section>
       </div>
     </div>
 
